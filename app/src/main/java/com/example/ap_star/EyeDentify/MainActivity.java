ae7af77f756eb3d43a4d509b3d2bbb4bbcf723af
sachi.alagiya <sachi.alagiya@gmail.com>
2019-02-27 23:22:39 -0600

    initial commit

---
+++ app/src/main/java/com/example/ap_star/EyeDentify/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.ap_star.EyeDentify;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.ap_star.EyeDentify.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bitmap mBitmap= BitmapFactory.decodeResource(getResources(),R.drawable.statue);
+        ImageView imageView=(ImageView)findViewById(R.id.imageView);
+        Button btnProcess=(Button)findViewById(R.id.btnProcess);
+
+        imageView.setImageBitmap(mBitmap);
+
+
+
+    }
+
+}
+
+
+
