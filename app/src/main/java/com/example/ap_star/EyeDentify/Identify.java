97eed94cda81b0275f87bb6fb5e1c6fae07425f1
ramyavajja <kodali1006>
2019-04-18 11:06:31 -0500

    Revert "Added gallery actions"
    This reverts commit 77ccba4

--- app/src/main/java/com/example/ap_star/EyeDentify/Identify.java
+++ app/src/main/java/com/example/ap_star/EyeDentify/Identify.java
@@ -1,71 +1,30 @@
 package com.example.ap_star.EyeDentify;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class Identify extends AppCompatActivity {
-
-    private static final int PICK_IMAGE=100;
-    Uri imageUri;
-    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        Button bt_cam=(Button)findViewById(R.id.btn_camera);
-        Button bt_gal=(Button)findViewById(R.id.btn_gallery);
-        //imgView =(ImageView)findViewById(R.id.imageView2);
-
-        bt_cam.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                Intent int1=new Intent(Identify.this ,cameraTest.class);
-                startActivity(int1);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-
-        bt_gal.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-
-                //Intent int2=new Intent(Identify.this ,Result.class);
-               // startActivity(int2);
-                openGallery();
-
-            }
-
-
-        });
-
-    }
-
-
-    private void openGallery(){
-
-        Intent int2=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(int2,PICK_IMAGE);
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(resultCode==RESULT_OK && requestCode==PICK_IMAGE){
-            imageUri=data.getData();
-            //imgView.setImageURI(imageUri);
-
-            Intent next =new Intent(this,Result.class);
-            next.putExtra("imageUri",imageUri.toString());
-            startActivity(next);
-            this.finish();
-
-        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
